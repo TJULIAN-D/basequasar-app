c60070b022689f2ff84e5992c583165c4cad50fb
TJULIAN-D <juliandavidbonillatorres@gmail.com>
2026-07-02 14:38:56 -0500

    Remove outdated precache manifest and update service worker to reference new manifest

--- dist/pwa/service-worker.js
+++ dist/pwa/service-worker.js
@@ -1,4 +1,4 @@
-importScripts("firebase-messaging-sw.js", "precache-manifest.1782093f8e0ff92dee1aff2508b023ac.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("firebase-messaging-sw.js", "precache-manifest.95947c7fab4d6508f95541f960a6293e.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /*
  * This file (which will be your service worker)
